a65e50325480a3801b78584dcfabc2f0263c14d7
Joao1V <joaovm.dev@gmail.com>
2025-03-17 14:36:26 -0400

    feat: add new params

--- src/hooks/useGetData.ts
+++ src/hooks/useGetData.ts
@@ -4,13 +4,13 @@ import { useMemo } from 'react';
 
 import { usePathname, useRouter } from 'next/navigation';
 
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery, useQueryClient } from '@tanstack/react-query';
 import index from 'libs/axios';
 
 interface OptionsProps {
    url: string;
    queryKey?: Array<any>;
-   filterKey?: string ;
+   filterKey?: string;
    onSuccess?: (response: any) => any | void;
    initialFn?: (serverData: any) => any | void; //A FUNCAO VAI RODAR APENAS UMA VEZ NA PRIMEIRA MONTAGEM DO COMPONENTE, O OBJETIVO PRINCIPAL É PARA FILTRAR OS DADOS QUE VIER DO SERVERDATA
    enabled?: boolean | null;
@@ -18,6 +18,7 @@ interface OptionsProps {
    serverData?: any;
    isKeepPrevious?: boolean;
    isQueryString?: boolean;
+   staleTime?: number | undefined;
 }
 
 interface MyQueryOptionsProps {
@@ -26,17 +27,21 @@ interface MyQueryOptionsProps {
    placeholderData?: any;
    enabled?: boolean;
    initialData?: any;
+   staleTime?: number | undefined;
 }
 
 export type GetDataResponse = {
    data: any;
-   filterKey: string ;
+   filterKey: string;
    error: unknown | null;
    isEmptyData: boolean;
    isError: boolean;
    isFetching: boolean;
    isLoading: boolean;
    refetch: () => void;
+   setData: (newData: any, isPaginate?: boolean) => void;
+   setDataUnique: (label: string, value: any) => void;
+   promise: Promise<any>
 };
 
 export const useGetData = (options: OptionsProps): GetDataResponse => {
@@ -51,13 +56,14 @@ export const useGetData = (options: OptionsProps): GetDataResponse => {
       params,
       isQueryString,
       serverData,
+      staleTime,
    } = options;
 
    const isServer = typeof window === 'undefined';
 
    const router = useRouter();
    const pathname = usePathname();
-
+   const queryClient = useQueryClient();
    const getDataFn = async () => {
       try {
          if (isQueryString) {
@@ -80,12 +86,38 @@ export const useGetData = (options: OptionsProps): GetDataResponse => {
       }
    };
 
+   const setDataUnique = (label: string, value: any) => {
+      queryClient.setQueryData(queryKey || [url], (oldData: any) => {
+         return {
+            ...oldData,
+            [label]: value,
+         };
+      });
+   };
+
+   const setData = async (newData: any, isPaginate?: boolean) => {
+      queryClient.setQueryData(queryKey || [url], (oldData: any) => {
+         if (isPaginate) {
+
+            return {
+               ...oldData,
+              data: newData
+            };
+         }
+         return {
+            ...oldData,
+            ...newData,
+         };
+      });
+   };
+
    const queryOptions: MyQueryOptionsProps = useMemo(
       () => ({
          queryKey: queryKey || [url],
          queryFn: getDataFn,
          placeholderData: isKeepPrevious ? keepPreviousData : undefined,
          enabled: enabled === null ? true : enabled,
+         staleTime: staleTime,
          ...(serverData ?
             {
                initialData:
@@ -105,9 +137,21 @@ export const useGetData = (options: OptionsProps): GetDataResponse => {
       [options],
    );
 
-   const { data, isLoading, refetch, isFetching, isError, error } = useQuery(queryOptions);
+   const { data, isLoading, refetch, isFetching, isError, error, promise } = useQuery(queryOptions);
 
    const isEmptyData = data?.data?.length === 0;
 
-   return { data, isLoading, refetch, isFetching, isEmptyData, isError, error, filterKey };
+   return {
+      data,
+      isLoading,
+      refetch,
+      isFetching,
+      isEmptyData,
+      isError,
+      error,
+      filterKey,
+      promise,
+      setData,
+      setDataUnique,
+   };
 };
