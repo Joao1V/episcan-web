7362690ee280c5cd64998762b42afea09b3d162b
Joao1V <joaovm.dev@gmail.com>
2025-03-17 15:07:06 -0400

    feat: actions edit e remove

--- src/app/(restrict)/(panel)/painel/camera/page.jsx
+++ src/app/(restrict)/(panel)/painel/camera/page.jsx
@@ -3,22 +3,24 @@
 import { Button } from '@components/ui';
 import { KTIcon } from 'kt-icon';
 
-import { ModalAddCamera } from '@/features/(panel)/modal/modal-add-camera';
+import { ModalCamera } from '@/features/(panel)/modal/modal-camera';
+import { MODAL_PANEL_KEYS } from '@/features/(panel)/modal/modalKeys';
 import { TableCompanyCameras } from '@/features/(panel)/table/table-company-cameras';
 import { useModal } from '@/hooks';
-import { MODAL_PANEL_KEYS } from '@/features/(panel)/modal/modalKeys';
+import { useCameraPaginate } from '@/services/queries/cameras';
 
 export default function Page() {
    const { setModal } = useModal(MODAL_PANEL_KEYS.ADD_CAMERA);
+   const { data } = useCameraPaginate();
 
    return (
       <>
-         <ModalAddCamera />
+         <ModalCamera />
          <div>
             <div className="d-flex flex-wrap flex-stack mb-6">
                <h3 className="fw-bold my-2 fs-5">
                   Câmeras
-                  <span className="fs-7 text-gray-500 fw-semibold ms-1">(0)</span>
+                  <span className="fs-7 text-gray-500 fw-semibold ms-1">({data?.total || ''})</span>
                </h3>
 
                <div className="d-flex flex-wrap my-2">
--- src/features/(panel)/table/table-company-cameras.tsx
+++ src/features/(panel)/table/table-company-cameras.tsx
@@ -1,22 +1,103 @@
 'use client';
 
+import { useMutation } from '@tanstack/react-query';
+import api from 'api';
+import { KTIcon } from 'kt-icon';
 import moment from 'moment-timezone';
 
+import { MODAL_PANEL_KEYS } from '@/features/(panel)/modal/modalKeys';
+import { useModal } from '@/hooks';
 import { TableBuilder } from '@/libs/table-builder';
 import { useCameraPaginate } from '@/services/queries/cameras';
-import { KTIcon } from 'kt-icon';
+import { useMonitoredCompany } from '@/services/queries/monitored-company';
+import { Button } from '@components/ui';
 
 export function TableCompanyCameras() {
-   const { data } = useCameraPaginate();
-   console.log('camera', data);
 
+   const { setModal } = useModal(MODAL_PANEL_KEYS.ADD_CAMERA);
+
+   const { data, isLoading, setData } = useCameraPaginate();
+   const { data: monitoredCompany } = useMonitoredCompany();
+
+   const { mutate: onEdit, isPending } = useMutation({
+      mutationFn: async (variables: {
+         identifier: string;
+         active: boolean;
+         index: number;
+         original: Record<string, any>;
+      }) => {
+         const { identifier, active, original } = variables;
+         const payload = {
+            monitored_company_identifier: monitoredCompany.identifier,
+            monitored_company_department_identifier:
+               original.monitored_company_department_identifier,
+            title: original.title,
+            url: original.url,
+            verification_minute: original.verification_minute,
+            active,
+         };
+
+         const aux = data?.data;
+         aux[variables.index].active = active;
+         setData(aux, true);
+         await api.put(`/restrict/camera/${identifier}`, payload, { isDisableToast: true });
+      },
+   });
+
+   const { mutate: onRemove, isPending: isPendingRemove } = useMutation({
+      mutationFn: async (variables: {
+         identifier: string;
+      }) => {
+         const { identifier } = variables;
+
+         await api.delete(`/restrict/camera/${identifier}`, null);
+      },
+   });
+
+   if (isLoading) return <div className={'text-center'}>Carregando...</div>;
    if (data?.data?.length === 0) {
       return <div className={'text-center'}>Nenhuma camera cadastrada</div>;
    }
+
    return (
       <TableBuilder
-         data={data?.data}
+         data={data ? [...data?.data] : []}
+         columnVisibility={{
+            identifier: false,
+         }}
          columns={[
+            {
+               accessorKey: 'identifier',
+               header: 'ID',
+            },
+            {
+               accessorKey: 'active',
+               header: 'Ativo',
+               cell: ({ cell, row, getValue }) => {
+                  return (
+                     <div className="form-check form-switch form-check-custom form-check-solid me-10 ">
+                        <input
+                           className="form-check-input h-20px w-30px cursor-pointer"
+                           type="checkbox"
+                           disabled={isPending}
+                           value=""
+                           checked={getValue()}
+                           onChange={() => {
+                              onEdit({
+                                 identifier: row.getValue('identifier'),
+                                 active: !getValue(),
+                                 original: row.original,
+                                 index: row.index,
+                              });
+                           }}
+                           id={cell.id}
+                        />
+                        <label className="form-check-label" htmlFor={cell.id}></label>
+                     </div>
+                  );
+               },
+               size: 80,
+            },
             {
                accessorKey: 'title',
                header: 'Camera',
@@ -33,7 +114,12 @@ export function TableCompanyCameras() {
             },
             {
                accessorKey: 'verification_minute',
-               header: () => <>Intervalo <br/>(em minutos)</>,
+               header: () => (
+                  <>
+                     Intervalo <br />
+                     (em minutos)
+                  </>
+               ),
                cell: (info) => {
                   return <div>{info.getValue()}</div>;
                },
@@ -58,12 +144,41 @@ export function TableCompanyCameras() {
                header: 'Ações',
                meta: {
                   isStick: true,
-
                },
                size: 80,
-               cell: () => <div className={'ps-4'}>
-                  <KTIcon name={'dots-horizontal'} type={'solid'} className={'fs-3'}/>
-               </div>,
+               cell: ({ row }) => (
+                  <div className={' d-flex flex-center gap-2'}>
+                     <Button
+                        className={'btn btn-reset p-0'}
+                        onClick={() => {
+                           setModal(true, {
+                              ...row.original,
+                              camera_identifier: row.original.identifier,
+                           });
+                        }}
+                        tooltip={{
+                           content: 'Editar',
+                           anchorSelect: `#edit_${row.index}`,
+                           place: 'bottom',
+                        }}
+                     >
+                        <KTIcon name={'pencil'} type={'solid'}/>
+                     </Button>
+
+                     <Button
+                        className={'btn btn-reset p-0'}
+                        onClick={() => onRemove({identifier: row.original.identifier})}
+                        disabled={isPendingRemove}
+                        tooltip={{
+                           content: 'Remover',
+                           anchorSelect: `#remove_${row.index}`,
+                           place: 'bottom',
+                        }}
+                     >
+                        <KTIcon name={'abstract-11'} type={'solid'}/>
+                     </Button>
+                  </div>
+               ),
             },
          ]}
       />
