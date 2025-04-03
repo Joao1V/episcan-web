1645a27882a1bccb977ef8734e94b9c46ded829a
Joao1V <joaovm.dev@gmail.com>
2025-04-03 11:46:21 -0400

    feat: organizado keys

--- src/services/queries/queryKeys.ts
+++ src/services/queries/queryKeys.ts
@@ -1,19 +1,30 @@
 export const QUERY_KEYS = {
-   ORGANIZATION_PAGINATE: 'restrict/organization/paginate',
-   ORGANIZATION_INVITE_USERS_PAGINATE: 'restrict/organization-invite/paginate',
-   ORGANIZATION_USERS_PAGINATE: (identifier: string) => `restrict/organization/${identifier}/customer/paginate`,
-
-   COMPANIES_PAGINATE: 'restrict/monitored-company/paginate',
-   DEPARTMENT_COMPANY_PAGINATE: (identifier: string) =>  `restrict/monitored-company/${identifier}/department/tree`,
-   CAMERAS_PAGINATE: 'restrict/camera/paginate',
-   EPIS_PAGINATE: 'restrict/epi-item/paginate',
-   ACTIVE_COMPANY: 'active_company',
-   ACTIVE_ORGANIZATION: 'active_organization',
+   ORGANIZATION: {
+      PAGINATE: 'organization_paginate',
+      INVITE_USERS_PAGINATE: 'organization_invite_users_paginate',
+      USERS_PAGINATE: 'organization_users_paginate',
+      ACTIVE: 'organization_active',
+   },
+   MONITORED_COMPANIES: {
+      PAGINATE: 'monitored_company_paginate',
+      DEPARTMENTS: 'monitored_company_departments_tree',
+      ACTIVE: 'monitored_company_active',
+   },
+   CAMERAS_PAGINATE: 'paginate_camera',
+   EPIS_PAGINATE: 'paginate_epi_item',
 
    CAMERA_VERIFICATION_PAGINATE: (identifier: string) => `latest_occurrences_${identifier}`,
    DASHBOARD_SUMMARY: 'dashboard_summary',
+
+   PERMISSIONS: 'permissions'
 };
 
 export const FILTER_KEYS = {
    LATEST_OCCURRENCES: 'latest_occurrences',
+};
+
+export const COOKIES_KEYS = {
+   ORGANIZATION: {
+      ACTIVE: 'organization_active',
+   },
 };
