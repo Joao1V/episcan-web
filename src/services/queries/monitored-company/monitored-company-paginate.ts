d4bd83d936191d950ef06ec02661deb84fbdf328
Joao1V <joaovm.dev@gmail.com>
2025-04-14 12:29:08 -0400

    feat: onSuccess prop

--- src/services/queries/monitored-company/monitored-company-paginate.ts
+++ src/services/queries/monitored-company/monitored-company-paginate.ts
@@ -1,17 +1,19 @@
 'use client';
 
+import { useParams } from 'next/navigation';
+
+import { useQueryClient } from '@tanstack/react-query';
+
 import type { MonitoredCompany } from '@/services/queries/monitored-company/types';
 import { QUERY_KEYS } from '@/services/queries/queryKeys';
 
 import type { PaginateResponse, ResponsePaginate } from '@/libs/axios/types';
 
 import { useGetData } from '@/hooks';
-import { useEffect } from 'react';
-import { useQueryClient } from '@tanstack/react-query';
-import { useParams } from 'next/navigation';
 
 interface Options {
    enabled?: boolean;
+   onSuccess?: (response: ResponsePaginate<MonitoredCompany[]>) => Promise<void> | void;
 }
 
 export function useMonitoredCompanyPaginate(options?: Options) {
@@ -20,16 +22,18 @@ export function useMonitoredCompanyPaginate(options?: Options) {
 
    const { data, refetch, isFetching } = useGetData<
       ResponsePaginate<MonitoredCompany[]>,
-      PaginateResponse<MonitoredCompany>
+      PaginateResponse<MonitoredCompany[]>
    >({
       queryKey: [QUERY_KEYS.MONITORED_COMPANIES.PAGINATE],
       url: 'restrict/monitored-company/paginate',
       onSuccess: async (response) => {
+         if (options?.onSuccess) {
+            await options.onSuccess(response.object);
+         }
          return response.object;
       },
       enabled: options?.enabled || false,
    });
-
 
    return {
       data,
