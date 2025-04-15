415f8b716429442387bb15612c5662208dbe3c14
Joao1V <joaovm.dev@gmail.com>
2025-04-15 10:27:24 -0400

    feat: redirect to /painel se logado

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,21 @@
 import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
 export default withAuth(
-   (req) => {
+   async (req) => {
       const requestHeaders = new Headers(req.headers);
       requestHeaders.set('x-url', req.url);
 
+      const token = await getToken({ req: req, secret: process.env.NEXTAUTH_SECRET });
+
+      const isLoggedIn = !!token;
+      const url = req.nextUrl;
+      // Se estiver logado e estiver na home (/), redireciona para /painel, deixei if desativo pq middlware nao roda.
+      if (isLoggedIn && url.pathname === '/') {
+         const redirectUrl = new URL('/painel', req.url);
+         return NextResponse.redirect(redirectUrl);
+      }
       return NextResponse.next({
          request: {
             headers: requestHeaders,
