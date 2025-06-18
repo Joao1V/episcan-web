fc8573a23a7ca004088fa9f63eca8f55fd02449a
Joao1V <joaovm.dev@gmail.com>
2025-06-18 11:53:13 -0400

    fix: error build

--- src/services/queries/cameras/types/index.ts
+++ src/services/queries/cameras/types/index.ts
@@ -2,31 +2,33 @@ type OptionsPaginate<TFilters> = {
    filterKey: string;
    initialFilters?: TFilters;
    isKeepPrevious?: boolean;
-}
+};
 
 interface CameraVerification {
-    camera_verification_verification_at: string;
-    camera_verification_verification_image1: string;
-    camera_verification_verification_image2: string;
-    camera_verification_verification_image3: string;
-    camera_verification_suspected_infraction: boolean;
-    bounding_box_suspected_true_count: number;
-    bounding_box_suspected_false_count: number;
-    camera_identifier: string;
-    camera_title: string;
-    camera_note: string | null;
-    camera_url: string;
-    camera_verification_minute: number;
-    camera_active: boolean;
-    camera_created_at: string;
-    monitored_company_name: string;
-    monitored_company_juridical_fancy_name: string;
-    monitored_company_cpfcnpj: string;
-    monitored_company_contact_mail: string;
-    monitored_company_contact_mobile_phone: string;
-    monitored_company_contact_business_phone: string | null;
-    monitored_company_department_title: string;
-    monitored_company_department_full_path: string;
+   active: boolean;
+   camera_verification_verification_at: string;
+   camera_verification_verification_image1: string;
+   camera_verification_verification_image2: string;
+   camera_verification_verification_image3: string;
+   camera_verification_suspected_infraction: boolean;
+   bounding_box_suspected_true_count: number;
+   bounding_box_suspected_false_count: number;
+   camera_identifier: string;
+   camera_title: string;
+   camera_note: string | null;
+   camera_url: string;
+   camera_verification_minute: number;
+   camera_active: boolean;
+   camera_created_at: string;
+   monitored_company_name: string;
+   monitored_company_juridical_fancy_name: string;
+   monitored_company_cpfcnpj: string;
+   monitored_company_contact_mail: string;
+   monitored_company_contact_mobile_phone: string;
+   monitored_company_contact_business_phone: string | null;
+   monitored_company_department_title: string;
+   monitored_company_department_full_path: string;
+   identifier: string;
 }
 
 export type { CameraVerification, OptionsPaginate };
